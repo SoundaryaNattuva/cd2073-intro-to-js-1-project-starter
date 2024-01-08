3080febccb2f1ec758f7c1b2b19f43ad7948b8b0
SoundaryaNattuva <soundaryanatts@gmail.com>
2024-01-07 22:42:23 -0500

    complete all exercises

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,21 +1,51 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 
-/* Create 3 or more product objects using object literal notation 
-   Each product should include five properties
-   - name: name of product (string)
-   - price: price of product (number)
-   - quantity: quantity in cart should start at zero (number)
-   - productId: unique id for the product (number)
-   - image: picture of product (url string)
-*/
+const products = [
+  {
+    name: 'Matcha Milk Tea',
+    price: 4.50,
+    quantity: 0,
+    productId: 2300,
+    image:'https://carmyy.com/wp-content/uploads/2022/01/matcha-bubble-tea-new.jpg'
+  },
+  
+  { 
+    name: 'Thai Milk Tea',
+    price: 4.50,
+    quantity: 0,
+    productId: 2301,
+    image:'https://cdn.pickuplimes.com/cache/71/9d/719dc2dc8fdb222a57bc5a86d772baf1.jpg'
+  },
 
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
+  {
+    name: 'Brown Sugar Milk Tea',
+    price: 5.00,
+    quantity: 0,
+    productId: 2302,
+    image: 'https://images.squarespace-cdn.com/content/v1/58d1f7308419c23d328cdb22/1559894022209-RV13I0JU6Y5N4P7XZKQU/Brown+Sugar+Bubble+Tea'
+  },
+  
+  {
+    name: 'Taro Milk Tea',
+    price: 5.00,
+    quantity: 0,
+    productId: 2303,
+    image: 'https://yummyaddiction.com/wp-content/uploads/2022/05/taro-bubble-tea-featured.jpg'
+  },
+
+    {
+    name: 'Oreo Milk Tea',
+    price: 6.00,
+    quantity: 0,
+    productId: 2304,
+    image: 'https://www.thatcutedish.com/wp-content/uploads/2022/02/oreo-bubble-shake-1.jpg'
+  },
+];
+
+// ----------------------------------
 
 /* Declare an empty array named cart to hold the items in the cart */
+const cart = []
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
@@ -23,10 +53,33 @@
   - if the product is not already in the cart, add it to the cart
 */
 
+function addProductToCart (productOfInterest){
+  let productOfInterestId = ''
+  for (let i=0; i<products.length; i++){
+    if (products[i].productId === productOfInterest){
+      products[i].quantity += 1
+      if (cart.includes(products[i].name) === false){
+        cart.push(products[i].name)
+      }
+    }
+  }
+}
+
+
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+
+function increaseQuantity(productId){
+  let productOfInterestId = ''
+  for (let i=0; i<products.length; i++){
+    if (products[i].productId === productOfInterest){
+      products[i].quantity += 1
+    }
+  }
+}
+
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
@@ -34,11 +87,46 @@
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
 
+function decreaseQuantity(productId){
+  let productOfInterestId = ''
+  for (let i=0; i<products.length; i++){
+    if (products[i].productId === productId){
+      products[i].quantity -= 1
+      console.log(products[i].quantity)
+      if (products[i].quantity === 0){
+        let removeItem = ''
+        removeItem = cart.indexOf(products[i].name)
+        cart.splice(removeItem, 1)
+      }
+    }
+  }
+}
+
+// decreaseQuantity(2304)
+// console.log(cart)
+
+
+
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+
+function removeProductFromCart(productId){
+  for (let i=0; i<products.length; i++){
+    if (products[i].productId === productId){
+      products[i].quantity === 0
+      let removeItem = ''
+      removeItem = cart.indexOf(products[i].name)
+      cart.splice(removeItem, 1)
+    }
+  }
+}
+
+// decreaseQuantity(2304)
+// console.log(cart, products[4].quantity)
+
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total cost of all products
@@ -46,7 +134,26 @@
   Hint: price and quantity can be used to determine total cost
 */
 
+function cartTotal(){
+  let totalCost = 0
+  cart.forEach((item) => {
+    for (let i=0; i<products.length; i++){
+      if (products[i].name === item){
+        let total = products[i].quantity * products[i].price
+        totalCost += total
+      }
+    }
+  })
+  return totalCost
+}
+
+
 /* Create a function called emptyCart that empties the products from the cart */
+
+function emptyCart(){
+  cart = []
+}
+
 
 /* Create a function named pay that takes in an amount as an argument
   - amount is the money paid by customer
@@ -55,7 +162,23 @@
   Hint: cartTotal function gives us cost of all the products in the cart  
 */
 
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
+function pay(amount){
+  let totalCost = cartTotal()
+  let balance = amount - totalCost
+  return balance
+}
+
+
+/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)
+
+Remove all items from the cart.
+Integrate a currency switcher to switch between USD, EUR, and YEN.
+Implement currency formatting to accomodate USD, EUR, and YEN.
+Come back once you're familiar with the DOM API, HTML, and CSS and try the following:
+Change/update the formatting of the store.
+Add a mock credit card form with form validation.
+Create a form for adding more products.
+*/
 
 
 /* The following is for running unit tests. 
@@ -74,6 +197,4 @@ module.exports = {
    cartTotal,
    pay, 
    emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
 }
